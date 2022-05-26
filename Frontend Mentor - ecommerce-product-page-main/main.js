cc7af89ce7f8ff9d9759e571f75ee27d72d6dc80
Patrick Messiter <patrick.messiter@gmail.com>
2022-05-26 16:08:37 +1000

    26.5.22 Commit - Updated JS logic for Desktop
    JS logic updated to show cart container, cart value and toggle through photos in lightbox mode.

--- Frontend Mentor - ecommerce-product-page-main/main.js
+++ Frontend Mentor - ecommerce-product-page-main/main.js
@@ -1,3 +1,5 @@
+/* MAIN IMAGE SECTION */
+
 let listOne = document.querySelector('#listOne');
 let listTwo = document.querySelector('#listTwo');
 let listThree = document.querySelector('#listThree');
@@ -7,19 +9,32 @@ let mainImage = document.querySelector('.mainImage');
 
 let listArray = [listOne, listTwo, listThree, listFour];
 
+
+
+let lightBoxContainer = document.querySelector('.lightBoxGallery');
+let lightBoxClose = document.querySelector('.closeLightBoxButton');
+let lightBoxPrevious = document.querySelector('.previousButton');
+let lightBoxNext = document.querySelector('.nextButton')
+
+let lightBoxMainImage = document.querySelector('.lightBoxGallery .mainImage');
+
 function mainImageSelector () {
     switch(true) {
         case listOne.checked:
             mainImage.src = "images/image-product-1.jpg";
+            lightBoxMainImage.src = "images/image-product-1.jpg";
             break;
         case listTwo.checked:
             mainImage.src = "images/image-product-2.jpg";
+            lightBoxMainImage.src = "images/image-product-2.jpg";
             break;
         case listThree.checked:
             mainImage.src = "images/image-product-3.jpg";
+            lightBoxMainImage.src = "images/image-product-3.jpg";
             break;
         case listFour.checked:
             mainImage.src = "images/image-product-4.jpg";
+            lightBoxMainImage.src = "images/image-product-4.jpg";
             break;
     }
 }
@@ -35,10 +50,6 @@ listFour.addEventListener('click', mainImageSelector);
 
 /*LIGHT BOX SECTION */
 
-let lightBoxContainer = document.querySelector('.lightBoxGallery');
-let lightBoxClose = document.querySelector('.closeLightBoxButton');
-
-
 function openLightBox () {
     lightBoxContainer.style.display = 'flex';
 }
@@ -51,6 +62,35 @@ function closeLightBox () {
 
 lightBoxClose.addEventListener('click', closeLightBox);
 
+function previousPhoto () {
+    for (i=0 ; i < listArray.length; i++) {
+        if (listArray[i].checked == true && listArray[i - 1] >= listArray[0]) {
+            listArray[i].checked = false;
+            listArray[i-1].checked = true;
+            mainImageSelector();
+        }
+    }
+}
+
+function nextPhoto () {
+    for (i= 3 ; i > -1; i--) {
+        if (listArray[i].checked == true && listArray[i + 1] <= listArray[3]) {
+            listArray[i].checked = false;
+            listArray[i+1].checked = true;
+            mainImageSelector();
+        }
+    }
+}
+
+function test () {
+    console.log(`1: ${listOne.checked} 2: ${listTwo.checked} 3: ${listThree.checked} 4: ${listFour.checked}`);
+}
+
+lightBoxPrevious.addEventListener('click', previousPhoto);
+lightBoxNext.addEventListener('click', nextPhoto);
+
+lightBoxMainImage.addEventListener('click', test);
+
 
 
 
@@ -61,6 +101,10 @@ let shoeTally = document.querySelector('.shoeTally');
 let shoeMinus = document.querySelector('#minusShoe');
 let shoeAdd = document.querySelector('#addShoe');
 
+let cartValueNumber = document.querySelector('#cartValueNumber');
+let addToCartButton = document.querySelector('.addToCartButton');
+
+
 let shoeValue = 0;
 
 function addShoe () {
@@ -68,13 +112,41 @@ function addShoe () {
     shoeTally.innerHTML = `${shoeValue}`;
 }
 
-function minusShoe() {
+function minusShoe () {
     if (shoeValue > 0) {
         shoeValue -= 1;
         shoeTally.innerHTML = `${shoeValue}`;
     }
 }
 
+function addToCart () {
+    cartValueNumber.style.display = "flex";
+    cartValueNumber.innerHTML = `${shoeValue}`;
+}
+
 shoeMinus.addEventListener('click', minusShoe);
 shoeAdd.addEventListener('click', addShoe);
+addToCartButton.addEventListener('click', addToCart);
+
+
+
+/* CART SECTION */
+
+let cartButton = document.querySelector('#cartButton');
+let cartContainer = document.querySelector('.cartContainer');
+let cartContainerContent = document.querySelector('.cartContainerContents');
+let cartContainerEmpty = document.querySelector('.cartContainerEmpty');
+
+
+
+function showCartContainer () {
+    cartContainer.style.display = "grid";
+    if (shoeValue == 0) {
+        cartContainerContent.style.display = "none";
+    } else {
+        cartContainerEmpty.style.display = "none";
+    }
+}
+
+cartButton.addEventListener('click', showCartContainer);
 
